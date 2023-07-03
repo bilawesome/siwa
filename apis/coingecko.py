332691f6849ec1b94c42ec7e351d89cdf2b845ce
Nicole Bilaw <nicolegbilaw@gmail.com>
2023-07-04 00:39:09 +0800

    Validate coingecko data with pydantic

--- apis/coingecko.py
+++ apis/coingecko.py
@@ -1,7 +1,46 @@
+import logging
+from pydantic import BaseModel, ValidationError
+
+from datetime import datetime
+from typing import List, Optional
 from typing import Any, Dict
 from apis.crypto_api import CryptoAPI
 import requests
 from apis import utils
+
+logger = logging.getLogger()
+
+
+class CoinGeckoData(BaseModel):
+    id: str
+    symbol: str
+    name: str
+    image: str
+    current_price: float
+    market_cap: int
+    market_cap_rank: int  # 12
+    # fully_diluted_valuation: int  # 6289106594
+    # total_volume: int  # 339054287
+    # high_24h: float  # 75.5
+    # low_24h: float  # 73.01
+    # price_change_24h: float  # 1.43
+    # price_change_percentage_24h: float
+    # market_cap_change_24h: int
+    # market_cap_change_percentage_24h: float
+    # circulating_supply: float
+    # total_supply: int
+    # max_supply: int
+    # ath: float
+    # ath_change_percentage: float
+    # ath_date: datetime
+    # atl: float
+    # atl_change_percentage: float
+    # atl_date: datetime
+    # roi: Optional[Dict] = None
+    last_updated: datetime
+
+    # class Config:
+    #     extra = "forbid"
 
 
 class CoinGeckoAPI(CryptoAPI):
@@ -17,6 +56,7 @@ class CoinGeckoAPI(CryptoAPI):
         extract_market_cap(data: Dict[str, Any]) -> Dict[float, Dict[str, str]]:
             Extracts market cap data from API response.
     """
+
     VS_CURRENCY = "usd"
     ORDER = "market_cap_desc"
     PAGE = 1
@@ -29,10 +69,11 @@ class CoinGeckoAPI(CryptoAPI):
     def __init__(self) -> None:
         """
         Constructs all the necessary attributes for the CoinGeckoAPI object.
+
+        Link to API documentation: https://www.coingecko.com/en/api/documentation
         """
         super().__init__(
-            url="https://api.coingecko.com/api/v3/coins/markets",
-            source='coingecko'
+            url="https://api.coingecko.com/api/v3/coins/markets", source="coingecko"
         )
 
     @utils.handle_request_errors
@@ -68,6 +109,9 @@ class CoinGeckoAPI(CryptoAPI):
             Dict[float, Dict[str, str]]:
                 A dictionary with market cap as keys and coin details as values.
         """
+
+        self.validate_api_data(data)
+
         market_data = {}
         for coin in data:
             name = coin[self.NAME_KEY]
@@ -78,3 +122,12 @@ class CoinGeckoAPI(CryptoAPI):
                 "last_updated": last_updated,
             }
         return market_data
+
+    def validate_api_data(self, data: List[CoinGeckoData]):
+        """Validate data returned by external API."""
+        for coin in data:
+            try:
+                CoinGeckoData(**coin)
+            except ValidationError as e:
+                logger.warning(e)
+                continue
